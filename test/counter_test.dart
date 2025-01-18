3c318dbbb55ec42b52e49ad22bde596bdb395a88
Mohamed Yasser <mohamedhcjdivdjvy@gmail.com>
2025-01-18 17:20:56 +0200

    first commit

---
+++ test/counter_test.dart
@@ -0,0 +1,34 @@
+import 'package:unit_test_tautorial/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Counter Test", () {
+    late Counter counter;
+
+    setUp(() {
+      counter = Counter();
+    });
+
+    test("intial value should be Zero", () {
+      expect(counter.count, 0);
+    });
+
+    test("Counter value should be increment", () {
+      counter.increment();
+      expect(counter.count, 1);
+    });
+
+    test("Counter value should be decrement", () {
+      counter.increment();
+      counter.decrement();
+
+      expect(counter.count, 0);
+    });
+
+    test("Counter value should be below Zero", () {
+      counter.decrement();
+
+      expect(counter.count, -1);
+    });
+  });
+}
